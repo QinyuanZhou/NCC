eccc4556082ca32ef6616e47a385b46c1004ef84
QinyuanZhou <32949874+QinyuanZhou@users.noreply.github.com>
2018-03-06 11:36:55 +0800

    Add files via upload
    test

---
+++ test.m
@@ -0,0 +1,10 @@
+%ªÒ»°I∫ÕT
+clc;
+clear;
+template_rgb=imread('TT.png');
+src_rgb=imread('II.png');
+template=rgb2gray(template_rgb);
+src=rgb2gray(src_rgb);
+T=im2double(template);
+I=im2double(src);
+
